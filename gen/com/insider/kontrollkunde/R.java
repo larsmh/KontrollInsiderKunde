f783f7fee99b41d8b81382bbd81d238bbd2dc7e2
larsmh <lars.mh23@gmail.com>
2014-10-02 17:19:28 +0200

    r-class

--- gen/com/insider/kontrollkunde/R.java
+++ gen/com/insider/kontrollkunde/R.java
@@ -1088,6 +1088,7 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
+        public static final int action_log_out=0x7f050045;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
@@ -1249,10 +1250,11 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int action_log_out=0x7f0a0010;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
-        public static final int title_activity_login=0x7f0a0010;
+        public static final int title_activity_login=0x7f0a0011;
     }
     public static final class style {
         /** 
